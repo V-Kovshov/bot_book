0d65488fe034bfdd909c0b5864d0cca09c0d40ca
Vadim Kovshov <ssevenn41@gmail.com>
2023-02-25 18:38:12 +0200

    update bot.py and keyboards/main_menu.py

--- bot.py
+++ bot.py
@@ -9,12 +9,13 @@ from keyboards.main_menu import set_main_menu
 # Инициализируем логгер
 logger = logging.getLogger(__name__)
 
+
 # Функция конфигурирования и запуска бота
 async def main():
     # Конфигурируем логирование
     logging.basicConfig(level=logging.INFO,
                         format=u'%(filename)s:%(lineno)d #%(levelname)-8s '
-                                u'[%(asctime)s] - %(name)s - %(message)s')
+                               u'[%(asctime)s] - %(name)s - %(message)s')
     # Выводим в консоль информацию о начале запуска бота
     logger.info('Starting bot')
 
