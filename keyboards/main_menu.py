f38cc6f703a644d44a42e56123afb6b79882d5ce
Vadim Kovshov <ssevenn41@gmail.com>
2023-02-25 16:43:00 +0200

    update some files

---
+++ keyboards/main_menu.py
@@ -0,0 +1,13 @@
+from aiogram import Bot, Daspatcher
+from aiogram.types import BotCommand
+
+from lexicon.lexicon import LEXICON_COMMANDS
+
+
+# Функция для настройки кнопки MENU
+async def set_main_manu(bot: Bot):
+    main_menu_commands = [BotCommand(command=command,
+                                    description=description) for command, description
+                                    in LEXICON_COMMANDS.items()]
+    await bot.set_my_commands(main_menu_commands)
+    
---
+++ services/file_handling.py
@@ -0,0 +1,35 @@
+BOOK_PATH = 'book/book.txt'
+PAGE_SIZE = 1050
+# BOOK_PATH = 'bot_book/services/book.txt'  # VS Code
+
+book: dict[int, str] = {}
+
+
+# Функция, возвращающая строку с текстом страницы и ее размер
+def _get_part_text(text: str, start: int, size: int) -> tuple[str, int]:
+    end = min(start + size, len(text))
+    text = text[start:end]
+    if len(text) <= 1:
+        return text, len(text)
+    if text[-1] in ',.!:;?' and text[-2] in ',.!:;?':
+        text = text[:len(text) - 2]
+    while text[-1] not in ',.!:;?':
+        text = text[:len(text) - 1]
+
+    return text, len(text)
+
+
+# Функция, формирующая словарь книги
+def prepare_book(path: str) -> None:
+    with open(path) as file:
+        book_txt = file.read()
+    page_num = 1
+    while len(book_txt) >= 2:
+        page_text, char = _get_part_text(book_txt, 0, PAGE_SIZE)
+        book[page_num] = page_text.lstrip()
+        book_txt = book_txt[char:]
+        page_num += 1
+
+
+# Вызов функции prepare_book для подготовки книги из текстового файла
+prepare_book(BOOK_PATH)
